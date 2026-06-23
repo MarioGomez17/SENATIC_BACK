21c1e0377fe0735a57aed0b728f086269e3ba68f
MarioGomez17 <mg441236@gmail.com>
2026-06-23 01:23:01 -0500

    fix: Se corrigieron algunos nombres de clases para que coincidan con el nombre del archivo

--- APLICACION_USUARIOS/serializers/write/TipoIdentificacion/SerializadorCrearTipoIdentificacion.py
+++ APLICACION_USUARIOS/serializers/write/TipoIdentificacion/SerializadorCrearTipoIdentificacion.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from APLICACION_USUARIOS.models import ModeloTipoIdentificacion
 
-class SerializadorCreacionTipoIdentificacion(serializers.ModelSerializer):
+class SerializadorCrearTipoIdentificacion(serializers.ModelSerializer):
 
     class Meta:
         model = ModeloTipoIdentificacion
