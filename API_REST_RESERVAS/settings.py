ec5b52c6e95c0992c18490794b13ec941a77f71c
MarioGomez17 <mg441236@gmail.com>
2026-06-23 09:48:10 -0500

    feat: Se agrego la configuración para que Swagger trabaje con autenticaciones en la UI

--- API_REST_RESERVAS/settings.py
+++ API_REST_RESERVAS/settings.py
@@ -109,5 +109,7 @@ SPECTACULAR_SETTINGS = {
     "TITLE": "API Sistema de Reservas",
     "DESCRIPTION": "Documentación de la API del sistema de reservas",
     "VERSION": "1.0.0",
-    "SERVE_INCLUDE_SCHEMA": False,
+    "SERVE_INCLUDE_SCHEMA": False,"SWAGGER_UI_SETTINGS": {
+        "persistAuthorization": True,
+    }
 }
