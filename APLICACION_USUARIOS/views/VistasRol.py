363e6917a2c3c3609762429129c6238ea3e44de2
MarioGomez17 <mg441236@gmail.com>
2026-06-22 23:37:33 -0500

    feat: Se creo el conjunto de vistas para el modelo de Rol

---
+++ APLICACION_USUARIOS/views/VistasRol.py
@@ -0,0 +1,82 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+
+from APLICACION_USUARIOS.models import ModeloRol
+from APLICACION_USUARIOS.services.ServicioRol import ServicioRol
+from APLICACION_USUARIOS.serializers.read.Rol import SerializadorListaRol, SerializadorDetalleRol
+from APLICACION_USUARIOS.serializers.write.Rol import SerializadorCrearRol, SerializadorActualizarRol
+
+class VistasRol(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet
+    ):
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return SerializadorListaRol
+        if self.action == "retrieve":
+            return SerializadorDetalleRol
+        if self.action == "create":
+            return SerializadorCrearRol
+        if self.action == "update":
+            return SerializadorActualizarRol
+
+    queryset = ModeloRol.objects.all()
+
+    # ===========================================
+    # LISTAR (GET /rol/)
+    # ===========================================
+    def list(self, request):
+        Roles = ServicioRol.ObtenerRoles()
+        Serializador = SerializadorListaRol(Roles, many=True)
+        return Response(Serializador.data, status=status.HTTP_200_OK)
+    
+    # ===========================================
+    # OBTENER (GET /rol/{id}/)
+    # ===========================================
+    def retrieve(self, request, pk=None):
+        Rol = ServicioRol.ObtenerRolPorId(pk)
+        Serializador = SerializadorDetalleRol(Rol)
+        return Response(Serializador.data, status=status.HTTP_200_OK)
+    
+    # ===========================================
+    # CREAR (POST /rol/)
+    # ===========================================
+    def create(self, request):
+        Serializador = SerializadorCrearRol(data=request.data)
+        Serializador.is_valid(raise_exception=True)
+        Rol = ServicioRol.CrearRol(Serializador.validated_data)
+        SerializadorRespuesta = SerializadorDetalleRol(Rol)
+        return Response(SerializadorRespuesta.data, status=status.HTTP_201_CREATED)
+
+    # ===========================================
+    # ACTUALIZAR COMPLETO (PUT /rol/{id}/)
+    # ===========================================
+    def update(self, request, pk=None):
+        Rol = ServicioRol.ObtenerRolPorId(pk)
+        Serializador = SerializadorActualizarRol(Rol, data=request.data)
+        Serializador.is_valid(raise_exception=True)
+        Rol = ServicioRol.ActualizarRol(Rol,Serializador.validated_data)
+        return Response(SerializadorDetalleRol(Rol).data, status=status.HTTP_200_OK)
+
+    # ===========================================
+    # ACTIVAR (PATCH /rol/{id}/)
+    # ===========================================
+    def partial_update(self, request, pk=None):
+        Rol = ServicioRol.ObtenerRolPorId(pk)
+        ServicioRol.ActivarRol(Rol)
+        return Response({"Mensaje" : "Rol Activado"}, status=status.HTTP_200_OK)
+
+    # ===========================================
+    # ELIMINAR (DELETE /rol/{id}/)
+    # ===========================================
+    def destroy(self, request, pk=None):
+        Rol = ServicioRol.ObtenerRolPorId(pk)
+        ServicioRol.EliminarRol(Rol)
+        return Response({"Mensaje" : "Rol Eliminado"}, status=status.HTTP_200_OK)
